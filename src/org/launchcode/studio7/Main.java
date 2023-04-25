91dbc9e55a50d1b34194d61b0b48803e8f5d8dcb
Victoria M <vmorris517@gmail.com>
2023-04-24 19:55:03 -0500

    completed studio debugging

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args){
 
-        CD cd = new CD("CD Example", 700, "CD-R", 350);
-        DVD dvd = new DVD("DVD Example", 4700, "DVD-R", 1450);
+        CD cd = new CD("Bratz Soundtrack", 700, "CD-R", 350);
+        DVD dvd = new DVD("The Godfather II", 4700, "DVD-R", 1450);
 
         cd.spinDisc();
         dvd.spinDisc();
